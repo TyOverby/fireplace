f333de32eb171364934aa7d74c5a85771347a75e
Ty Overby <ty@pre-alpha.com>
2017-09-10 12:40:03 -0700

    updated

--- src/scripts/components/Hello.tsx
+++ src/scripts/components/Hello.tsx
@@ -4,7 +4,7 @@ export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
-export class Hello extends React.Component<HelloProps, undefined> {
+export class Hello extends React.Component<HelloProps> {
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
